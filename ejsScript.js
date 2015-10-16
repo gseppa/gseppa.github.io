6a851bb7877fded19c3b6652343968dc9b894eb6
gseppa <graham.seppa@gmail.com>
2015-10-16 12:21:57 -0400

    javascript

---
+++ ejsScript.js
@@ -0,0 +1,11 @@
+var navWrap = document.getElementsByClassName("navwrap");
+
+var navButtons = document.getElementsByClassName("navbuttons");
+
+var logo = document.getElementById("logo");
+
+var emailForm = document.getElementById("emailForm");
+
+
+
+
